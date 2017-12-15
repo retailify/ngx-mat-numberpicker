5aa5dfca76b549c9b9884ca6c742a2df082c4d00
Tim Meitz <info@danios.net>
2017-12-15 01:14:28 +0100

    version upgrade

--- src/number-picker.component.ts
+++ src/number-picker.component.ts
@@ -1,25 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngx-mat-numberpicker',
-  templateUrl: './number-picker.component.html'
+  templateUrl: './number-picker.component.html',
 })
-export class NumberPickerComponent {
+export class NumberPickerComponent implements OnInit {
 
-    numberPickerValue: number;
+    private numberPickerValue: number;
+    private decreaseButtonDisabled: boolean = false;
+    private increaseButtonDisabled: boolean = false;
+
+    @Input() disabled: boolean = false;
+    @Input() min: number = 0;
+    @Input() max: number = 50;
+    @Input() default: number;
+
+    @Output() onChange: EventEmitter<number> = new EventEmitter();
 
     constructor() {
         this.numberPickerValue = 0;
     }
 
+    ngOnInit(): void {
+        if (this.default === null || this.default === undefined) {
+           this.numberPickerValue = 0;
+        } else {
+            this.numberPickerValue = this.default;
+        }
+        this.checkButtons();
+    }
+
     increase(event: any): void {
         event.preventDefault();
-        this.numberPickerValue++;
+        if (this.numberPickerValue < this.max) {
+            this.numberPickerValue++;
+            this.onChange.emit(this.numberPickerValue);
+        }
+        this.checkButtons();
     }
 
     decrease(event: any): void {
         event.preventDefault();
-        this.numberPickerValue--;
+        if (this.numberPickerValue > this.min) {
+            this.numberPickerValue--;
+            this.onChange.emit(this.numberPickerValue);
+        }
+        this.checkButtons();
+    }
+
+    checkButtons(): void {
+        this.increaseButtonDisabled = false;
+        this.decreaseButtonDisabled = false;
+        if (this.numberPickerValue <= this.min) {
+            this.decreaseButtonDisabled = true;
+        }
+        if (this.numberPickerValue >= this.max) {
+            this.increaseButtonDisabled = true;
+        }
     }
 
 }
